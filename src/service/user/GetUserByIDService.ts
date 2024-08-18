63f02c3c9ea98f0def5d8c788718e0fa975a39a4
ruhangaratini <garatini.ruhan@gmail.com>
2024-08-18 16:41:11 -0300

    :zap: Adjustment get user by id

--- src/service/user/GetUserByIDService.ts
+++ src/service/user/GetUserByIDService.ts
@@ -2,8 +2,8 @@ import { UserEntity } from "../../model/entity/UserEntity";
 import { ErrorCode } from "../../model/ErrorCode";
 import { UserRepository } from "../../repository/UserRepository";
 
-export async function getUserByIDService(personID: string): Promise<UserEntity | ErrorCode> {
+export async function getUserByIDService(userID: string): Promise<UserEntity | ErrorCode> {
     const repository = UserRepository.getInstance();
 
-    return await repository.getByID(personID);
+    return await repository.getByID(userID);
 }
